c5ed6113624650e6f5a857c5498ec2e8c74de68b
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-10-17 20:30:10 -0400

    Bump to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dicom_info',
-    version='1.0.2',
+    version='1.0.4',
     description='A ChRIS DS plugin that generates various reports based on the tag information in a DICOM file.',
     author='FNNDSC',
     author_email='dev@babyMRI.org',
