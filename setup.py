3bb28b3a25e6c0678c0a5bc7449d220122e35a82
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-11-11 15:24:44 -0500

    Fix minor typo and bump to 1.0.28

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dicom_info',
-    version='1.0.26',
+    version='1.0.28',
     description='A ChRIS DS plugin that generates various reports based on the tag information in a DICOM file.',
     author='FNNDSC',
     author_email='dev@babyMRI.org',
